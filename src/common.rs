7992e1f67293997d10ace2153b737a4eeef5b22f
jack1232 <jackjack5810@yahoo.com>
2023-09-04 10:30:21 -0400

    update to 0.17

--- src/common.rs
+++ src/common.rs
@@ -244,6 +244,7 @@ impl State {
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         if new_size.width > 0 && new_size.height > 0 {
+            self.init.instance.poll_all(true);
             self.init.size = new_size;
             self.init.config.width = new_size.width;
             self.init.config.height = new_size.height;
